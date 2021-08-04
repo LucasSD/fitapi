ce02ea406edb0751440e81a8fd289942565f72af
LucasSD <lucas.stonedrake@gmail.com>
2021-08-04 14:41:45 +0100

    add config and batch files for coverage

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -9,7 +9,6 @@ def test_page_uses_correct_template():
     with app.test_client() as test_client:
         response = test_client.get("/")
         assert response.status_code == 200
-        
         # assertTemplateUsed(response, "home.html")
 
 def test_html():
