75ed9fc6c67e6dc5f92cb5a1aefee0eb621d41ba
LucasSD <lucas.stonedrake@gmail.com>
2021-08-05 18:40:40 +0100

    link API to db. Errors because of two Flask app instances

--- fitapi/dailyInfo.py
+++ fitapi/dailyInfo.py
@@ -2,28 +2,8 @@ from datetime import datetime
 
 from flask import abort, make_response
 
-date1 = datetime(1984, 1, 1).strftime("%Y-%m-%d %H:%M:%S")
-date1a = datetime(1984, 1, 2).strftime("%Y-%m-%d %H:%M:%S")
-date2 = datetime(1985, 1, 1).strftime("%Y-%m-%d %H:%M:%S")
-date2a = datetime(1985, 1, 2).strftime("%Y-%m-%d %H:%M:%S")
-date3 = datetime(1986, 1, 1).strftime("%Y-%m-%d %H:%M:%S")
-date3a = datetime(1986, 1, 2).strftime("%Y-%m-%d %H:%M:%S")
-
-# Data to serve with our API
-DAILYINFO = {
-    date1: {
-        "startDate": date1,
-        "endDate": date1a,
-    },
-    date2: {
-        "startDate": date2,
-        "endDate": date2a,
-    },
-    date3: {
-        "startDate": date3,
-        "endDate": date3a,
-    },
-}
+from config import db
+from models import Daily, DailySchema
 
 # Create a handler for our read (GET) daily info
 def read_all():
@@ -31,10 +11,16 @@ def read_all():
     Responds to a request for /api/daily
     with the complete list of daily info
 
-    :return:        sorted list of daily info
+    :return:        json string of list of daily info
     """
     # Create the list of daily info from our data
-    return [DAILYINFO[key] for key in sorted(DAILYINFO.keys())]
+    daily = Daily.query \
+        .order_by(Daily.startDate) \
+        .all()
+
+    # Serialize the data for the response
+    daily_schema = DailySchema(many=True)
+    return daily_schema.dump(daily).data
 
 def read_one(startDate):
     """
@@ -43,18 +29,25 @@ def read_one(startDate):
     :param startDate:   event date
     :return:        daily event matching startDate
     """
+
+    # Get the date requested
+    day = Daily.query \
+        .filter(Daily.startDate == startDate) \
+        .one_or_none()
+
+
     # Does the date exist in daily info?
-    if startDate in DAILYINFO:
-        day = DAILYINFO.get(startDate)
+    if day is not None:
+        
+        # Serialize the data for the response
+        day_schema = DailySchema()
+        return day_schema.dump(day).data
 
     # if not found
     else:
         abort(
             404, "Date {startDate} not found".format(startDate=startDate)
         )
-
-    return day
-
 
 def create(day):
     """
@@ -63,60 +56,114 @@ def create(day):
     :param day:  day to create in daily info structure
     :return:        201 on success, 406 if day exists already
     """
-    startDate = day.get("startDate", None)
-    endDate = day.get("endDate", None)
+    startDate = day.get("startDate")
+    endDate = day.get("endDate")
 
-    # Does the day exist already?
-    if startDate not in DAILYINFO and startDate is not None:
-        DAILYINFO[startDate] = {
-            "startDate": startDate,
-            "endDate": endDate,
-        }
-        return DAILYINFO[startDate], 201
+    existing_day = Daily.query \
+        .filter(Daily.endDate == endDate) \
+        .filter(Daily.startDate == startDate) \
+        .one_or_none()
 
-    # day already exists
+    # Can we insert this day?
+    if existing_day is None:
+
+        # Create a daily instance using the schema and the passed-in day
+        schema = DailySchema()
+        new_day = schema.load(day, session=db.session).data
+
+        # Add the day to the database
+        db.session.add(new_day)
+        db.session.commit()
+
+        # Serialize and return the newly created day in the response
+        return schema.dump(new_day).data, 201
+
+    # Otherwise, nope, day exists already
     else:
-        abort(
-            406,
-            "Date {startDate} already exists".format(startDate=startDate),
-        )
+        abort(409, f'Date {startDate} exists already')
 
 
 def update(startDate, day):
     """
-    Updates an existing daily entry in the daily info structure
-    :param startDate:   date to update in the daily info structure
-    :param day:  date to update
-    :return:        updated daily entry
+    Updates an existing day in the daily info structure
+    Throws an error if a day with the date we want to update to
+    already exists in the database.
+    :param startDate:   startDate of the day to update in the daily info structure
+    :param day:      day to update
+    :return:            updated day structure
     """
-    # Does the date exist in daily info?
-    if startDate in DAILYINFO:
-        DAILYINFO[startDate]["endDate"] = day.get("endDate")
+    # Get the day requested from the db into session
+    update_day = Daily.query.filter(
+        Daily.startDate == startDate
+    ).one_or_none()
 
-        return DAILYINFO[startDate]
+    # Try to find an existing day with the same date as the update
+    endDate = day.get("endDate")
+    startDate = day.get("startDate")
 
-    # if does not exist
-    else:
+    existing_day = (
+        Daily.query.filter(Daily.endDate == endDate)
+        .filter(Daily.startDate == startDate)
+        .one_or_none()
+    )
+
+    # If day does not exist
+    if update_day is None:
         abort(
-            404, "Date {startDate} not found".format(startDate=startDate)
+            404,
+            "Date {startDate} not found".format(startDate=startDate),
         )
+
+    # Date already exists
+    elif (
+        existing_day is not None and existing_day.startDate != startDate
+    ):
+        abort(
+            409,
+            "Entry {endDate} {startDate} exists already".format(
+                endDate=endDate, startDate=startDate
+            ),
+        )
+
+    else:
+
+        # turn the payload into a db object
+        schema = DailySchema()
+        update = schema.load(day, session=db.session)
+
+        # Set the startDate to the day we want to update
+        update.startDate = update_day.startDate
+
+        # merge the new object into the old and commit it to the db
+        db.session.merge(update)
+        db.session.commit()
+
+        # return updated day in the response
+        data = schema.dump(update_day)
+
+        return data, 200
 
 
 def delete(startDate):
     """
-    Deletes a date from the dailyInfo structure
-    :param startDate:   date to delete
-    :return:        200 on successful delete, 404 if not found
+    Deletes a day from the daily info structure
+    :param startDate:   startDate of the day to delete
+    :return:            200 on successful delete, 404 if not found
     """
-    # Does the date exist?
-    if startDate in DAILYINFO:
-        del DAILYINFO[startDate]
+    # Get the day requested
+    day = Daily.query.filter(Daily.startDate == startDate).one_or_none()
+
+    # Day exists?
+    if day is not None:
+        db.session.delete(day)
+        db.session.commit()
         return make_response(
-            "{startDate} successfully deleted".format(startDate=startDate), 200
+            "Date {startDate} deleted".format(startDate=startDate), 200
         )
 
-    # date not found
+    # Day does not exist
     else:
         abort(
-            404, "Date {startDate} not found".format(startDate=startDate)
+            404,
+            "Date {startDate} not found".format(startDate=startDate),
         )
