e8a31c352133a8a105ac339a32e3748ebe5b6729
LucasSD <lucas.stonedrake@gmail.com>
2021-08-07 19:21:28 +0100

    setup test_db in test_client()

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,25 +5,38 @@ import pytest
 from fitapi import create_app
 from fitapi.models import Daily
 from flask_sqlalchemy import SQLAlchemy
+from fitapi import db
 
 
 @pytest.fixture(scope='module')
 def test_client():
-    test_app = create_app(test=True)
+    test_app = create_app()
  
     # Flask provides a way to test your application by exposing the Werkzeug test Client
     # and handling the context locals for you.
     testing_client = test_app.test_client()
+
+    test_app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
  
     # Establish an application context before running the tests.
     ctx = test_app.app_context()
     ctx.push()
+    with ctx:
+        db.create_all()
+        # Insert Daily data
+        day1 = Daily(startDate=datetime(1984, 1, 1), endDate=datetime(1984, 1, 2))
+        day2 = Daily(startDate=datetime(1985, 1, 1), endDate=datetime(1985, 1, 2))
+        db.session.add(day1)
+        db.session.add(day2)
+ 
+        # Commit the changes to db
+        db.session.commit()
  
     yield testing_client  # this is where the testing happens!
  
     ctx.pop()
 
-@pytest.fixture(scope='module')
+'''@pytest.fixture(scope='module')
 def init_database():
     # Create the database and the database table
     test_db = SQLAlchemy()
@@ -40,7 +53,7 @@ def init_database():
  
     yield test_db  # testing
  
-    test_db.drop_all()
+    test_db.drop_all()'''
 
 @pytest.fixture(scope='module')
 def new_daily():
--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -3,11 +3,11 @@ import connexion
 from fitapi.models import Daily
 from datetime import datetime
 
-def test_swagger_ui_url(test_client, init_database):
+def test_swagger_ui_url(test_client):
     response = test_client.get('/api/ui/')
     assert response.status_code == 200
 
-def test_daily_url(test_client, init_database):
+def test_daily_url(test_client):
     response = test_client.get('/api/daily')
     assert response.status_code == 200
 
@@ -15,9 +15,9 @@ def test_daily_model(new_daily):
     assert str(new_daily.startDate) == '1984-01-01 00:00:00'
     assert str(new_daily.endDate) == '1984-01-02 00:00:00'
 
-def test_get_all_daily(test_client, init_database):
+def test_get_all_daily(test_client):
     url = "/api/daily"
-    expected_json = []
+    expected_json = [{'day_id': None,'endDate': '1984-01-02T00:00:00','startDate': '1984-01-01T00:00:00'},{'day_id': None,'endDate': '1985-01-02T00:00:00','startDate': '1985-01-01T00:00:00'}]
     response = test_client.get(url)
     assert response.json == expected_json
 
