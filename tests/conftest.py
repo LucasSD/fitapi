990d8f228089a66b7d859060314f44e4bdaa0229
LucasSD <lucas.stonedrake@gmail.com>
2021-08-09 17:53:10 +0100

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,16 +23,27 @@ def test_client():
     ctx.push()
     with ctx:
         db.create_all()
+
+        # Insert User data
+        user1 = User(fname="Lucas", lname="Stone-Drake")
+        user2 = User(fname="Tony", lname="Stark")
+        db.session.add(user1)
+        db.session.add(user2)
+
         # Insert Daily data
-        day1 = Daily(startDate=datetime(1984, 1, 1), endDate=datetime(1984, 1, 2))
-        day2 = Daily(startDate=datetime(1985, 1, 1), endDate=datetime(1985, 1, 2))
-        db.session.add(day2)
+        day1 = Daily(startDate=datetime(1984, 1, 1), endDate=datetime(1984, 1, 2), user_id=user2.user_id)
+        day2 = Daily(startDate=datetime(1985, 1, 1), endDate=datetime(1985, 1, 2), user_id=user2.user_id)
         db.session.add(day1)
+        db.session.add(day2)
+
+        user = User.query.filter_by(lname="Stark").first()
+        user.dates = [day1]
+    
 
         # Commit the changes to db
         db.session.commit()
 
-    yield testing_client  # this is where the testing happens!
+    yield testing_client  # testing here
 
     ctx.pop()
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -18,34 +18,48 @@ def test_daily_model(new_daily):
     assert str(new_daily.startDate) == "1984-01-01 00:00:00"
     assert str(new_daily.endDate) == "1984-01-02 00:00:00"
 
+def test_dates_url(test_client):
+    response = test_client.get("/api/dates")
+    assert response.status_code == 200
+
+def test_one_user_one_date_url(test_client):
+    response = test_client.get("/api/daily/2/date/01-01-1984")
+    assert response.status_code == 200
+
 
 def test_user_model(new_user):
     assert str(new_user.fname) == "Tony"
     assert str(new_user.lname) == "Stark"
 
 
-def test_get_all_daily_and_ordering(test_client):
+def test_get_all_users_and_ordering_by_lname(test_client):
     url = "/api/daily"
 
-    # 1985 date was added to db first
     expected_json = [
-        {"endDate": "1984-01-02T00:00:00", "startDate": "1984-01-01T00:00:00"},
-        {"endDate": "1985-01-02T00:00:00", "startDate": "1985-01-01T00:00:00"},
+        {'dates': [{'endDate': '1984-01-02 00:00:00','startDate': '1984-01-01 00:00:00','user_id': 2}],'fname': 'Tony','lname': 'Stark','user_id': 2},{'dates': [], 'fname': 'Lucas', 'lname': 'Stone-Drake', 'user_id': 1}
     ]
     response = test_client.get(url)
     assert response.json == expected_json
 
 
-def test_get_one_valid_daily(test_client):
-    response = test_client.get("/api/daily/01-01-1984")
+def test_get_one_valid_user(test_client):
+    response = test_client.get("/api/daily/1")
     assert response.status_code == 200
     expected_json = {
-        "endDate": "1984-01-02T00:00:00",
-        "startDate": "1984-01-01T00:00:00",
+'dates': [], 'fname': 'Lucas', 'lname': 'Stone-Drake', 'user_id': 1
     }
     assert response.json == expected_json
 
 
-def test_get_one_invalid_daily(test_client):
-    response = test_client.get("/api/daily/01-01-2000")
+def test_get_one_invalid_user(test_client):
+    response = test_client.get("/api/daily/15")
     assert response.status_code == 404
+
+def test_get_all_dates_and_ordering_by_startdate(test_client):
+    url = "/api/dates"
+
+    expected_json = [
+        {'endDate': '1985-01-02T00:00:00','startDate': '1985-01-01T00:00:00','user': None}, {'endDate': '1984-01-02T00:00:00','startDate': '1984-01-01T00:00:00','user': {'fname': 'Tony', 'lname': 'Stark', 'user_id': 2}}
+    ]
+    response = test_client.get(url)
+    assert response.json == expected_json
